11af352fc7c79d2a942ec47a3c32ad794600bd12
lheng8 <101233831+lheng8@users.noreply.github.com>
2022-08-27 12:29:30 +0700

    push

---
+++ likepage_footer.php
@@ -0,0 +1,58 @@
+<?php 
+echo '<div class="global_footer ">
+<div class="left_footer_text">© 2022 Eventbrite</div>
+<div class="middle-alignment">
+    <div class="middle_footer_text remove_dot_li">
+
+        <li>
+            <a style="padding-right: 10px"> About </a>
+        </li>
+        <li>
+            <a style="padding-right: 10px"> Blog </a>
+        </li>
+        <li>
+            <a style="padding-right: 10px"> Help </a>
+        </li>
+        <li>
+            <a style="padding-right: 10px"> Careers </a>
+        </li>
+        <li>
+            <a style="padding-right: 10px"> Press </a>
+        </li>
+        <li>
+            <a style="padding-right: 10px"> Investor </a>
+        </li>
+        <li>
+            <a style="padding-right: 10px"> Security </a>
+        </li>
+        <li>
+            <a style="padding-right: 10px"> Developer </a>
+        </li>
+        <li>
+            <a style="padding-right: 10px"> Terms </a>
+        </li>
+        <li>
+            <a style="padding-right: 10px"> Privacy </a>
+        </li>
+        <li>
+            <a style="padding-right: 10px"> PrivacyNotice </a>
+        </li>
+        <li>
+            <a style="padding-right: 10px"> Accesibility </a>
+        </li>
+        <li>
+            <a style="padding-right: 10px"> Cookies </a>
+        </li>
+
+    </div>
+</div>
+
+<div class="right_footer_text" style="float: right;">
+    <div>
+        Cambodia
+        <i class="fa-solid fa-chevron-down" style="padding-left: 5px;"></i>
+    </div>
+</div>
+
+</div>'
+?>
