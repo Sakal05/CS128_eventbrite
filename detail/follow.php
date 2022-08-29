70cb618a0f9b5646c9ec7faa9001210fd5b07ceb
Sakal <samnang.sakal@gmail.com>
2022-08-29 23:22:11 +0700

    add jol knong detail page

---
+++ detail/follow.php
@@ -0,0 +1,22 @@
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "eventbrite_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$id = $_POST['id'];
+$sql = "UPDATE event SET Like_status=1 WHERE Event_ID =" . $id;
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record updated successfully";
+} else {
+  echo "Error updating record: " . $conn->error;
+}
+$conn->close();
+?>
